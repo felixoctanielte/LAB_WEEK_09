82e0bafd2c5fa3ae286d82f20952b9450d81b26f
Felix Octaniel <felixoctanieltel05@gmail.com>
2025-11-09 16:57:45 +0700

    Commit No. 1 – Building Jetpack Compose UI

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -11,7 +11,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions
+
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.material3.TextField
 
